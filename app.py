64415ee3ab626d32936f5c8e280d8cc6218782de
Ionut Blanaru <blanaru.ionut@gmail.com>
2024-11-13 02:37:04 +0000

    Finalized the design of the products cards

--- app.py
+++ app.py
@@ -16,10 +16,17 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 @app.route("/")
+@app.route("/get_base")
+def get_base():
+    return render_template("base.html")
+
+
 @app.route("/get_desktops")
 def get_desktops():
     desktops = mongo.db.desktops.find()
     return render_template("desktops.html", desktops=desktops)
+
+
 
 
 if __name__ == "__main__":
